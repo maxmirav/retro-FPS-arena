07758a46a3e21ba291e298b7fe92063596db9274
Max Miravite <maxmiravite@gmail.com>
2019-08-26 07:10:03 +0800

    enemy scale to size and spin

---
+++ Assets/Scripts/EnemyBasicMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBasicMove : MonoBehaviour
+{
+    //Variables for ScaleToSize
+    private Vector3 startSize = Vector3.zero;
+    private Vector3 endSize = new Vector3(2, 2, 2);
+    private float lerpTime = 0;
+    [SerializeField] float scaleSpeed;
+
+    private float rotationSpeed = 270.0f;
+
+    private void Start()
+    {
+        StartCoroutine(RotateForSeconds());
+    }
+
+    private void Update()
+    {
+        ScaleToSize();
+    //https://answers.unity.com/questions/885972/how-to-stop-rotation-after-a-certain-limit.html
+
+    }
+
+    
+    private void ScaleToSize()
+    {
+        transform.localScale = Vector3.Lerp(startSize, endSize, lerpTime);
+        lerpTime += Time.deltaTime * scaleSpeed;
+    }
+
+    private IEnumerator RotateForSeconds() //Call this method with StartCoroutine(RotateForSeconds());
+    {
+        float time = 2.1f;     
+        while(time > 0)     
+        {
+            transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);   
+            time -= Time.deltaTime;    
+            yield return null;     
+        }
+}
+
+
+
+}
---
+++ Assets/Scripts/GenerateEnemies.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemies : MonoBehaviour
+{
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private int xPos, zPos;
+    [SerializeField] private int enemyCount;
+
+    private void Start()
+    {
+        StartCoroutine(EnemyDrop());
+
+    }
+
+    private IEnumerator EnemyDrop()
+    {
+        while (enemyCount < 10)
+        {
+            xPos = Random.Range(-40, 43);
+            zPos = Random.Range(-40, 43);
+            Instantiate(enemy, new Vector3 (xPos, 4, zPos), transform.rotation);
+            yield return new WaitForSeconds(0.1f);
+            enemyCount += 1;
+        }
+    }
+
+}
