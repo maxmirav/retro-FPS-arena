488a1590dfb5658aa433bb7e96861958f0168c5c
Max Miravite <maxmiravite@gmail.com>
2019-08-25 16:27:17 +0800

    finalized player movement

--- Assets/Scripts/Player Controller/PlayerMove.cs
+++ Assets/Scripts/Player Controller/PlayerMove.cs
@@ -5,6 +5,64 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private string horizontalInputName, verticalInputName;
+    [SerializeField] private float movementSpeed;
 
     private CharacterController charController;
+
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private KeyCode jumpKey;
+
+    private bool isJumping;
+
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        //Don't need time.deltaTime for these because SimpleMove already applies it under the hood
+        float horizInput = Input.GetAxis(horizontalInputName);
+        float vertInput = Input.GetAxis(verticalInputName);
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        //ClampMagnitude is like Mathf.Clamp but for Vector3 and you can only specify a maximum value
+        charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
+
+        JumpInput();
+    }
+
+    private void JumpInput()
+    {
+        if(Input.GetKeyDown(jumpKey) && !isJumping)
+        {
+            isJumping = true;
+            StartCoroutine(JumpEvent());
+        }
+    }
+
+    private IEnumerator JumpEvent()
+    {
+        charController.slopeLimit = 90.0f;
+        float timeInAir = 0.0f;
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir);
+            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime); //Move does not apply Time.deltaTime unlike in SimpleMove
+            timeInAir += Time.deltaTime;
+            yield return null;
+
+        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+
+        charController.slopeLimit = 90.0f;
+        isJumping = false;
+    }
 }
