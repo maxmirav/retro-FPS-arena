d39f25ae8f7f020a1eb8ed3d97484790043a5c56
Max Miravite <maxmiravite@gmail.com>
2019-08-25 11:56:29 +0800

    player look

---
+++ Assets/Scripts/Player Controller/PlayerMove.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName, verticalInputName;
+
+    private CharacterController charController;
+}
