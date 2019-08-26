994dbebc35c493f4223ae00642a61ff85c53140a
Max Miravite <maxmiravite@gmail.com>
2019-08-26 21:59:49 +0800

    implemented weapon sway

---
+++ Assets/Scripts/Player Controller/WeaponSway.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSway : MonoBehaviour
+{
+    [SerializeField] private float swayAmount;
+    [SerializeField] private float maxAmount;
+    [SerializeField] private float smoothAmount;
+
+    private Vector3 initialPosition;
+
+    private void Start()
+    {
+        initialPosition = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        float movementX = Input.GetAxis("Mouse X") * swayAmount;
+        float movementY = Input.GetAxis("Mouse Y") * swayAmount;
+
+        Vector3 finalPosition = new Vector3(movementX, movementY, 0);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, finalPosition + initialPosition, Time.deltaTime * smoothAmount);
+    }
+}
