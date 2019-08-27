493a522299df575c83e64a30f80148e491597771
Max Miravite <maxmiravite@gmail.com>
2019-08-27 13:29:28 +0800

    clamped weapon sway

--- Assets/Scripts/Player Controller/WeaponSway.cs
+++ Assets/Scripts/Player Controller/WeaponSway.cs
@@ -19,6 +19,8 @@ public class WeaponSway : MonoBehaviour
     {
         float movementX = Input.GetAxis("Mouse X") * swayAmount;
         float movementY = Input.GetAxis("Mouse Y") * swayAmount;
+        movementX = Mathf.Clamp(movementX, -maxAmount, maxAmount);
+        movementY = Mathf.Clamp(movementY, -maxAmount, maxAmount);
 
         Vector3 finalPosition = new Vector3(movementX, movementY, 0);
         transform.localPosition = Vector3.Lerp(transform.localPosition, finalPosition + initialPosition, Time.deltaTime * smoothAmount);
